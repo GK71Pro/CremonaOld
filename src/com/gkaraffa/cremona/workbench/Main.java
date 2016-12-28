5a50df8f440aa232d6e1488503fa9d98a7015c0e
gkaraffa <gkaraffa@icloud.com>
2016-12-28 12:52:40 -0500

    hotfix

--- src/com/gkaraffa/cremona/workbench/Main.java
+++ src/com/gkaraffa/cremona/workbench/Main.java
@@ -17,6 +17,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("This is the hotfix");
 		try {
 			ScaleFactory sF = new DiatonicScaleFactory();
 			Scale cScale = sF.createScale(DiatonicScaleFactory.ionianPattern, Tone.C);
