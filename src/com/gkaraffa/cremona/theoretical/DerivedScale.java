c31fa5ed98bd5dcf5aae10646a34c84fb4275a7f
gkaraffa <gkaraffa@icloud.com>
2017-05-01 12:28:12 -0400

    removed Derived Scale support

--- src/com/gkaraffa/cremona/theoretical/DerivedScale.java
+++
@@ -1,8 +0,0 @@
-package com.gkaraffa.cremona.theoretical;
-
-public class DerivedScale extends Scale {
-
-  public DerivedScale(String name, Tone[] tones, ScaleQuality scaleQuality) {
-    super(name, tones, scaleQuality);
-  }
-}
