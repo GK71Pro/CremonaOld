ded9aa1e803d1550a3fcd1e1d0fc1abfd9ea31a4
gkaraffa <gkaraffa@172.20.14.157>
2017-11-06 22:49:54 -0800

    Added experimental package w/ one class

---
+++ src/com/gkaraffa/cremona/experimenal/Frequency.java
@@ -0,0 +1,13 @@
+package com.gkaraffa.cremona.experimenal;
+
+public class Frequency {
+  private double cycles = 0.0;
+  
+  public Frequency(double cycles) {
+    this.cycles = cycles;
+  }
+
+  public double getCycles() {
+    return cycles;
+  } 
+}
